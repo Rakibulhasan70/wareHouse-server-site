cd75bc40981e3ccba323d7ed8edc9ef7092f3d36
Rakibulhasan70 <rakibulhasansohag5@gmail.com>
2022-04-29 21:40:27 +0600

    database connected

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 // middlewear
 
 app.use(cors());
 app.use(express.json());
+
+// wearhouseManage
+// jqY7tWDivZB0Sqx1
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.faflb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('connected');
+    // perform actions on the collection object
+    client.close();
+});
+
 
 
 app.get('/', (req, res) => {
